65a202c63dc9a31a695b39ef97ca8137dc49c99e
kyho22 <kyho35@hawaii.edu>
2018-07-20 11:10:47 -1000

    moved command parser function into new file

---
+++ prototype_watchman/prototype_watchman.sdk/lwip_udp_echo/src/cmd_parser.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "lwip/err.h"
+#include "lwip/udp.h"
+#define MAX_ARRAY_SIZE 100
+#include "xil_printf.h"
+
+char** command_buffer[MAX_ARRAY_SIZE];
+
+void command_parser(struct pbuf *p);
