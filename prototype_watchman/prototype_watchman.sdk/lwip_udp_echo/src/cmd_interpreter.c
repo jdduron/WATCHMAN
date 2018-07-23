527e7b3c9819535d0942efd84b299d61a8c1a575
kyho22 <kyho35@hawaii.edu>
2018-07-23 10:53:29 -1000

    Command buffer now send mult packets

--- prototype_watchman/prototype_watchman.sdk/lwip_udp_echo/src/cmd_interpreter.c
+++ prototype_watchman/prototype_watchman.sdk/lwip_udp_echo/src/cmd_interpreter.c
@@ -4,7 +4,5 @@ void command_interpreter(char** command_buffer){
 
 		printf("Command Interpreter ???\n");
 
-//        udp_sendto(pcb, p, addr, port);
-        /* free the pbuf */
-//        pbuf_free(p);
 }
+
