8c168e30b17ee9a67726e94b95634893b3e9ddb6
jdduron <40073656+jdduron@users.noreply.github.com>
2018-07-27 14:58:04 -1000

    created function that creates regmap and returns its address

---
+++ prototype_watchman/prototype_watchman.sdk/lwip_udp_echo/src/reg_map.h
@@ -0,0 +1,18 @@
+/*
+ * reg_map.h
+ *
+ *  Created on: Jul 27, 2018
+ *      Author: Jose Idlabs
+ */
+
+#ifndef SRC_REG_MAP_H_
+#define SRC_REG_MAP_H_
+#include <stdio.h>
+#include <string.h>
+#include "lwip/err.h"
+#include "lwip/udp.h"
+#include "xil_printf.h"
+
+int * reg_map();
+
+#endif /* SRC_REG_MAP_H_ */
