7599549aa15ce5acb756aeb45ddb443d215aadc2
jdduron <40073656+jdduron@users.noreply.github.com>
2018-07-31 15:12:37 -1000

    - created a parser that converts string data into int array in recieve.py
    -can output result into txt file

--- Python_Scripts/recieve.py
+++ Python_Scripts/recieve.py
@@ -12,6 +12,7 @@ import sys
 import socket
 import optparse
 
+
 #define the IP and port for the zynq
 UDP_IP = "192.168.1.10"
 UDP_PORT = 8
@@ -29,14 +30,33 @@ def main():
     sock = setup_connection()
     sock.bind(("", UDP_PORT))
 
-#    recv_flag = 0
+    recv_flag = 0
 
-#    while (recv_flag != -1):
-    while (1):
+    while (recv_flag != -1):
+#    while (1):
 
         data, server = sock.recvfrom(8192)
-#        if(data == 'end'):
-#            recv_flag = -1;
+        # if(data[-3:-1] == 'end'):
+        #     recv_flag = -1;
         print >>sys.stderr, 'recieved "%s"' % data
+        if data.startswith('head'):
+            if data[5:9]=='test':
+                A=data[10:-3]
+                B = [int(x) for x in A.split('/') if x.strip()]
+
+
+        outfile = open('test3', 'w')
+        count=0
+        for x in range(0,4):
+            for y in range(0,4):
+                for z in range(0,4):
+                    print 'Outfile B[%d]= %d' % (count, B[count])
+                    outfile.write("%d\n" % (B[count]))
+                    count +=1
+        outfile.close()
+        recv_flag =-1
+
+
+
 
 main()
